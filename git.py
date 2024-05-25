8ba008eb8318c27933840506b54a19224a5e938d
xayrullohyusupov <xayrullohyusupov757@gmail.com>
2024-05-25 18:07:51 +0500

    a

---
+++ git.py
@@ -0,0 +1,17 @@
+"""
+first:
+
+git init
+git add.
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/xayrullohyusupov/xash1.git
+git push -u origin main
+
+
+second:
+
+git add.
+git commit -m "..."
+git push
+"""
